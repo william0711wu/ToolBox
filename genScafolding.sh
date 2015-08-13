84641ecedee9a4265c331e0a5cc4f4d34eefeb23
william0711wu <wuwywu@gmail.com>
2015-08-13 15:51:48 +0800

    添加基础crud模板

---
+++ genScafolding.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#选择
+# 
+#
+#
+gradle scaffoldOfLeopaud  -PartifactId=example -PgroupId=com.duowan -PentityName=Example -Poverwrite=true -ProotProjectDir=/Users/wuwenyi/Downloads/example --stacktrace
